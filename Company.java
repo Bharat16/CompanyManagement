f5a0a5dec6f201e5d144b2ad3a037f9d9365c747
Bharat Bhargava <bh375119@dal.ca>
2019-11-18 16:24:37 -0400

    Upload New File

---
+++ Company.java
@@ -0,0 +1,13 @@
+import java.sql.*;
+import java.io.*;
+
+public class Company {
+
+	public static void main(String[] args) 
+	{
+		
+		DbOperations op=new DbOperations();
+		op.connectToSQL();		
+	}
+
+}
